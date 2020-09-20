af5c8560f7b76136ad01437f1fafb2ace84c8aa9
iceman <xin.liang@shopee.com>
2020-09-20 22:35:02 +0800

    feat: 修改mock control为动态返回

--- src/control/mock.js
+++ src/control/mock.js
@@ -1,13 +1,13 @@
-// 引入mockjs来模拟数据
 const Mock = require('mockjs')
-const data = Mock.mock({
-  'list|1-10': [{
-    'id|+1': 1
-  }]
-})
-const img = Mock.Random.image('200x100')
 
 module.exports = async(ctx, next) =>{
+  // 引入mockjs来模拟数据
+  const data = Mock.mock({
+    'list|1-10': [{
+      'id|+1': 1
+    }]
+  })
+  const img = Mock.Random.image('200x100')
   let mockData ={
     msg: 'mock hello api works',
     data: data,
